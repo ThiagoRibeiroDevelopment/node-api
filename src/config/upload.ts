5b71785aeea0777336bcf7d84bb1d70346686d0d
Thiago Ribeiro <thiagoth4@hotmail.com>
2022-01-05 17:00:56 -0300

    babel e serviço de email pela amazon

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,56 +2,42 @@ import path from 'path';
 import multer, { StorageEngine } from 'multer';
 import crypto from 'crypto';
 
-// interface IUploadConfig {
-//   driver: 's3' | 'disk';
-//   tmpFolder: string;
-//   directory: string;
-//   multer: {
-//     storage: StorageEngine;
-//   };
-//   config: {
-//     aws: {
-//       bucket: string;
-//     };
-//   };
-// }
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+  tmpFolder: string;
+  directory: string;
+  multer: {
+    storage: StorageEngine;
+  };
+  config: {
+    aws: {
+      bucket: string;
+    };
+  };
+}
 
 const uploadFolder = path.resolve(__dirname, '..', '..', 'uploads');
-// const tmpFolder = path.resolve(__dirname, '..', '..', 'temp');
+const tmpFolder = path.resolve(__dirname, '..', '..', 'temp');
 
 export default {
+  driver: process.env.STORAGE_DRIVER,
   directory: uploadFolder,
-  storage: multer.diskStorage({
-    destination: uploadFolder,
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('hex');
+  tmpFolder,
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('hex');
 
-      const filename = `${fileHash}-${file.originalname}`;
+        const filename = `${fileHash}-${file.originalname}`;
 
-      callback(null, filename);
+        callback(null, filename);
+      },
+    }),
+  },
+  config: {
+    aws: {
+      bucket: 'api-vendas',
     },
-  }),
-};
-
-// export default {
-//   driver: process.env.STORAGE_DRIVER,
-//   directory: uploadFolder,
-//   tmpFolder,
-//   multer: {
-//     storage: multer.diskStorage({
-//       destination: tmpFolder,
-//       filename(request, file, callback) {
-//         const fileHash = crypto.randomBytes(10).toString('hex');
-
-//         const filename = `${fileHash}-${file.originalname}`;
-
-//         callback(null, filename);
-//       },
-//     }),
-//   },
-//   config: {
-//     aws: {
-//       bucket: 'api-vendas',
-//     },
-//   },
-// } as IUploadConfig;
+  },
+} as IUploadConfig;
--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,13 +1,11 @@
 import AppError from '@shared/errors/AppError';
 import uploadConfig from '@config/upload';
-import path from 'path/posix';
-import fs from 'fs';
 import { inject, injectable } from 'tsyringe';
 import { IUpdateUserAvatar } from '../domain/models/IUpdateUserAvatar';
 import { IUser } from '../domain/models/IUser';
 import { IUsersRepository } from '../domain/repositories/IUsersRepository';
-// import DiskStorageProvider from '@shared/providers/StorageProvider/DiskStorageProvider';
-// import S3StorageProvider from '@shared/providers/StorageProvider/S3StorageProvider';
+import S3StorageProvider from '@shared/StorageProvider/S3StorageProvider';
+import DiskStorageProvider from '@shared/StorageProvider/DiskStorageProvider';
 
 @injectable()
 class UpdateUserAvatarService {
@@ -25,31 +23,22 @@ class UpdateUserAvatarService {
     if (!user) {
       throw new AppError('User not found.');
     }
-    if (user.avatar) {
-      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
 
-      if (userAvatarFileExists) {
-        await fs.promises.unlink(userAvatarFilePath);
+    if (uploadConfig.driver === 's3') {
+      const s3Provider = new S3StorageProvider();
+      if (user.avatar) {
+        await s3Provider.deleteFile(user.avatar);
       }
+      const filename = await s3Provider.saveFile(avatarFilename);
+      user.avatar = filename;
+    } else {
+      const diskProvider = new DiskStorageProvider();
+      if (user.avatar) {
+        await diskProvider.deleteFile(user.avatar);
+      }
+      const filename = await diskProvider.saveFile(avatarFilename);
+      user.avatar = filename;
     }
-
-    user.avatar = avatarFilename;
-    // if (uploadConfig.driver === 's3') {
-    //   const s3Provider = new S3StorageProvider();
-    //   if (user.avatar) {
-    //     await s3Provider.deleteFile(user.avatar);
-    //   }
-    //   const filename = await s3Provider.saveFile(avatarFilename);
-    //   user.avatar = filename;
-    // } else {
-    //   const diskProvider = new DiskStorageProvider();
-    //   if (user.avatar) {
-    //     await diskProvider.deleteFile(user.avatar);
-    //   }
-    //   const filename = await diskProvider.saveFile(avatarFilename);
-    //   user.avatar = filename;
-    // }
 
     await this.usersRepository.save(user);
 
